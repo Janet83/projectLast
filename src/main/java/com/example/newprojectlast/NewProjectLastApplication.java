5550dcacd44743ec65962b8f12d0bb94256dc4c6
Janet <zan.smigiel@gmail.com>
2023-01-14 09:58:20 +0100

    Zmiana dodanie nowych plików ze springinitializer z projektu tymczasowego

---
+++ src/main/java/com/example/newprojectlast/NewProjectLastApplication.java
@@ -0,0 +1,13 @@
+package com.example.newprojectlast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewProjectLastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewProjectLastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newprojectlast/NewProjectLastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newprojectlast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewProjectLastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
